ebf2c47599319a3b74c7f8355dce34511586e06d
dx3am <maksimtysuk5@email.com>
2025-03-20 16:31:29 +0200

    Інтегровано NumberGuesser у Program.cs

--- program.cs
+++ program.cs
@@ -4,18 +4,27 @@ class Program
 {
     static void Main()
     {
+        NumberGuesser game = new NumberGuesser(new Random().Next(1, 8));
+
         Console.WriteLine("Комп'ютер загадав число від 1 до 7. Твоє завдання — вгадати!");
 
         while (true)
         {
             Console.Write("Введи число: ");
             string? input = Console.ReadLine();
-            
-            if (string.IsNullOrEmpty(input))
+
+            if (string.IsNullOrEmpty(input))  
             {
                 Console.WriteLine("Будь ласка, введи число!");
                 continue;
             }
+
+            string response = game.Guess(input);
+
+            Console.WriteLine(response);
+
+            if (response.Contains("успіх"))
+                break;
         }
     }
 }
