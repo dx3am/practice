3fc1a72744bce9108e28a5ee73f53e3006f6a320
dx3am <maksimtysuk5@email.com>
2025-03-20 16:13:06 +0200

    Додано цикл взаємодії з користувачем

--- program.cs
+++ program.cs
@@ -5,5 +5,17 @@ class Program
     static void Main()
     {
         Console.WriteLine("Комп'ютер загадав число від 1 до 7. Твоє завдання — вгадати!");
+
+        while (true)
+        {
+            Console.Write("Введи число: ");
+            string? input = Console.ReadLine();
+            
+            if (string.IsNullOrEmpty(input))
+            {
+                Console.WriteLine("Будь ласка, введи число!");
+                continue;
+            }
+        }
     }
 }
