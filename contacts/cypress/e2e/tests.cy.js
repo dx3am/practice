f7fbfb007a6e18415efa2c15471410dc69c28d6c
dx3am <maksimtysuk5@gmail.com>
2025-03-26 12:49:40 +0200

    Оновлено tests.cy.js

--- contacts/cypress/e2e/tests.cy.js
+++ contacts/cypress/e2e/tests.cy.js
@@ -1,4 +1,4 @@
-describe("Додаток контактів - розширені тести", () => {
+describe("Додаток контактів - тести", () => {
   beforeEach(() => {
     cy.visit("http://localhost:5173");
   });
@@ -15,53 +15,51 @@ describe("Додаток контактів - розширені тести", ()
     cy.contains("li", "Андрій").should("not.exist");
   });
 
-  it("Додає 10 контактів і правильно їх сортує", () => {
-    const contacts = [
-      { name: "Юрій", phone: "900900900" },
-      { name: "Богдан", phone: "111222333" },
-      { name: "Олексій", phone: "444555666" },
-      { name: "Зоряна", phone: "777888999" },
-      { name: "Василь", phone: "123123123" },
-      { name: "Ганна", phone: "456456456" },
-      { name: "Тарас", phone: "789789789" },
-      { name: "Дмитро", phone: "321321321" },
-      { name: "Євген", phone: "654654654" },
-      { name: "Костянтин", phone: "987987987" }
-    ];
+  it("Додає контакт і відображає його у списку", () => {
+    cy.get('input[placeholder="Ім\'я"]').type("Максим");
+    cy.get('input[placeholder="Телефон"]').type("999888777");
+    cy.contains("button", "Додати").click();
+    cy.contains("li", "Максим - 999888777").should("be.visible");
+  });
 
-    contacts.forEach((contact) => {
-      cy.get('input[placeholder="Ім\'я"]').type(contact.name);
-      cy.get('input[placeholder="Телефон"]').type(contact.phone);
+  it("Редагує контакт і зберігає зміни", () => {
+    cy.get('input[placeholder="Ім\'я"]').type("Олена");
+    cy.get('input[placeholder="Телефон"]').type("555666777");
+    cy.contains("button", "Додати").click();
+
+    cy.contains("li", "Олена - 555666777").within(() => {
+      cy.contains("button", "Редагувати").click();
+    });
+
+    cy.get('input[placeholder="Ім\'я"]').clear().type("Олена Іванівна");
+    cy.get('input[placeholder="Телефон"]').clear().type("111222333");
+    cy.contains("button", "Редагувати").click();
+
+    cy.contains("li", "Олена Іванівна - 111222333").should("be.visible");
+  });
+
+  it("Видаляє контакт зі списку", () => {
+    cy.get('input[placeholder="Ім\'я"]').type("Сергій");
+    cy.get('input[placeholder="Телефон"]').type("123123123");
+    cy.contains("button", "Додати").click();
+
+    cy.contains("li", "Сергій - 123123123").within(() => {
+      cy.contains("button", "Видалити").click();
+    });
+
+    cy.contains("li", "Сергій - 123123123").should("not.exist");
+  });
+
+  it("Сортує контакти за іменем", () => {
+    const names = ["Ярослав", "Анна", "Богдан"];
+    names.forEach((name, index) => {
+      cy.get('input[placeholder="Ім\'я"]').type(name);
+      cy.get('input[placeholder="Телефон"]').type(`00000000${index}`);
       cy.contains("button", "Додати").click();
     });
 
     cy.contains("button", "Сортувати за іменем").click();
-    cy.get(".contacts-list li").first().should("contain", "Богдан");
-  });
-
-  it("Перевіряє, що список контактів не порожній після додавання", () => {
-    cy.get(".contacts-list").children().should("have.length", 0);
-
-    cy.get('input[placeholder="Ім\'я"]').type("Марина");
-    cy.get('input[placeholder="Телефон"]').type("888555444");
-    cy.contains("button", "Додати").click();
-
-    cy.get(".contacts-list").children().should("have.length.above", 0);
-  });
-
-  it("Дозволяє змінювати контакт без втрати даних", () => {
-    cy.get('input[placeholder="Ім\'я"]').type("Ірина");
-    cy.get('input[placeholder="Телефон"]').type("123321123");
-    cy.contains("button", "Додати").click();
-
-    cy.contains("li", "Ірина - 123321123").within(() => {
-      cy.contains("button", "Редагувати").click();
-    });
-
-    cy.get('input[placeholder="Ім\'я"]').clear().type("Ірина Петрова");
-    cy.contains("button", "Редагувати").click();
-
-    cy.contains("li", "Ірина Петрова - 123321123").should("be.visible");
+    cy.get(".contacts-list li").first().should("contain", "Анна");
   });
 
   it("Очищає поля після додавання контакту", () => {
